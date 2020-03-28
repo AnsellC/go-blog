75b6ab7373cabf26bdb2549c418bd22fa7bfa779
AnsellC <ansellcruz@gmail.com>
2020-03-29 07:08:09 +0800

    add http serve

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/ansellc/go-blog/database"
@@ -23,4 +25,9 @@ func (a *App) Run(port string) {
 func (a *App) Init() {
 	a.DB = database.Connect(os.Getenv("APP_DB_HOST"), os.Getenv("APP_DB_USERNAME"), os.Getenv("APP_DB_PASSWORD"), os.Getenv("APP_DB_DBNAME"), os.Getenv("APP_DB_PORT"))
 	a.Router = mux.NewRouter()
+}
+
+// Serve listen and serve the application using the port specified
+func (a *App) Serve(port string) {
+	log.Fatal(http.ListenAndServe(port, a.Router))
 }
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
 	a := App{}
 	a.Init()
+	a.Serve(os.Getenv("APP_HTTP_PORT"))
 }
